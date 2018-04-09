b49c460bd22842dc8b3dcaa32d24765c9628861a
Mark Norman Francis <norm@201created.com>
2018-04-09 22:51:26 +0100

    Ignore more files in defaults and shortcuts
    When applying macOS defaults and shortcuts, they will often come from
    a repository of their own. Trying to do something useful with some
    common base-of-repository files such as READMEs and licensing files
    isn't helpful, so we ignore them.
    
    Previously, the scripts would only ignore `README.markdown` as that was
    all I used in my repository. They now ignore some other common files.
    
    Thanks to @SteveMarshall for the original PR #8 that prompted this.

--- setup/apply_defaults.sh
+++ setup/apply_defaults.sh
@@ -4,27 +4,33 @@ if [ -d $dir ]; then
     pushd $dir >/dev/null  # unnecessarily noisy
 
     for file in *; do
-        if [ "$file" != 'README.markdown' ]; then
-            status "apply $file"
+        # fairly standard files in a repo that aren't defaults files
+        [ "$file" == 'README.markdown' ] && continue
+        [ "$file" == 'README.md' ]       && continue
+        [ "$file" == 'README.rst' ]      && continue
+        [ "$file" == 'LICENCE' ]         && continue
+        [ "$file" == 'LICENSE' ]         && continue
 
-            for line in $( cat "$file" ); do
-                # trim whitespace
-                line=$( echo "$line" | sed -e 's/^ *//' -e 's/ *$//' )
+        # otherwise...
+        status "apply $file"
 
-                case "$line" in
-                    \#*) ;;  # ignore commented lines
+        for line in $( cat "$file" ); do
+            # trim whitespace
+            line=$( echo "$line" | sed -e 's/^ *//' -e 's/ *$//' )
 
-                    KILL*)
-                        kill=$( echo $line | cut -c6- )
-                        killall $kill
-                        ;;
+            case "$line" in
+                \#*) ;;  # ignore commented lines
 
-                    *)
-                        eval defaults write "$file" "$line"
-                        ;;
-                esac
-            done
-        fi
+                KILL*)
+                    kill=$( echo $line | cut -c6- )
+                    killall $kill
+                    ;;
+
+                *)
+                    eval defaults write "$file" "$line"
+                    ;;
+            esac
+        done
     done
 
     popd >/dev/null  # unnecessarily noisy
--- setup/apply_shortcuts.sh
+++ setup/apply_shortcuts.sh
@@ -8,63 +8,69 @@ if [ -d $dir ]; then
     pushd $dir >/dev/null  # unnecessarily noisy
 
     for file in *; do
-        if [ "$file" != 'README.markdown' ]; then
-            status "apply $file"
+        # fairly standard files in a repo that aren't defaults files
+        [ "$file" == 'README.markdown' ] && continue
+        [ "$file" == 'README.md' ]       && continue
+        [ "$file" == 'README.rst' ]      && continue
+        [ "$file" == 'LICENCE' ]         && continue
+        [ "$file" == 'LICENSE' ]         && continue
 
-            if [ "$file" == 'global' ]; then
-                args='-globalDomain'
-            else
-                args="$file"
-            fi
+        # otherwise...
+        status "apply $file"
 
-            # redirect errors to /dev/null because on a new Mac there will
-            # be no NSUserKeyEquivalents, but this isn't an error for us
-            defaults delete $args NSUserKeyEquivalents 2>/dev/null || true
-
-            for line in $( cat "$file" ); do
-                case "$line" in
-                    \#*) # ignore comments
-                        ;;
-
-                    *)  menu_entry=$(
-                            echo "$line" \
-                                | awk -F '[[:space:]][[:space:]]+' \
-                                    '{ print $1 }'
-                        )
-                        shortcut=$(
-                            echo "$line" \
-                                | awk -F '[[:space:]][[:space:]]+' \
-                                    '{ print $2 }' \
-                                | perl -pe '
-                                    s{\bF1\b}{\\Uf704};
-                                    s{\bF2\b}{\\Uf705};
-                                    s{\bF3\b}{\\Uf706};
-                                    s{\bF4\b}{\\Uf707};
-                                    s{\bF5\b}{\\Uf708};
-                                    s{\bF6\b}{\\Uf709};
-                                    s{\bF7\b}{\\Uf70a};
-                                    s{\bF8\b}{\\Uf70b};
-                                    s{\bF9\b}{\\Uf70c};
-                                    s{\bF10\b}{\\Uf70d};
-                                    s{\bF11\b}{\\Uf70e};
-                                    s{\bF12\b}{\\Uf70f};
-                                    s{(cmd|command)[+-]}{@};
-                                    s{(alt|opt(ion)?)[+-]}{~};
-                                    s{shift[+-]}{\$};
-                                    s{(control|ctrl)[+-]}{^};
-                                    s{left}{\\U2190};
-                                    s{up}{\\U2191};
-                                    s{right}{\\U2192};
-                                    s{down}{\\U2193};
-                                    '
-                        )
-
-                        defaults write $args NSUserKeyEquivalents \
-                            -dict-add "$menu_entry" "$shortcut"
-                        ;;
-                esac
-            done
+        if [ "$file" == 'global' ]; then
+            args='-globalDomain'
+        else
+            args="$file"
         fi
+
+        # redirect errors to /dev/null because on a new Mac there will
+        # be no NSUserKeyEquivalents, but this isn't an error for us
+        defaults delete $args NSUserKeyEquivalents 2>/dev/null || true
+
+        for line in $( cat "$file" ); do
+            case "$line" in
+                \#*) # ignore comments
+                    ;;
+
+                *)  menu_entry=$(
+                        echo "$line" \
+                            | awk -F '[[:space:]][[:space:]]+' \
+                                '{ print $1 }'
+                    )
+                    shortcut=$(
+                        echo "$line" \
+                            | awk -F '[[:space:]][[:space:]]+' \
+                                '{ print $2 }' \
+                            | perl -pe '
+                                s{\bF1\b}{\\Uf704};
+                                s{\bF2\b}{\\Uf705};
+                                s{\bF3\b}{\\Uf706};
+                                s{\bF4\b}{\\Uf707};
+                                s{\bF5\b}{\\Uf708};
+                                s{\bF6\b}{\\Uf709};
+                                s{\bF7\b}{\\Uf70a};
+                                s{\bF8\b}{\\Uf70b};
+                                s{\bF9\b}{\\Uf70c};
+                                s{\bF10\b}{\\Uf70d};
+                                s{\bF11\b}{\\Uf70e};
+                                s{\bF12\b}{\\Uf70f};
+                                s{(cmd|command)[+-]}{@};
+                                s{(alt|opt(ion)?)[+-]}{~};
+                                s{shift[+-]}{\$};
+                                s{(control|ctrl)[+-]}{^};
+                                s{left}{\\U2190};
+                                s{up}{\\U2191};
+                                s{right}{\\U2192};
+                                s{down}{\\U2193};
+                                '
+                    )
+
+                    defaults write $args NSUserKeyEquivalents \
+                        -dict-add "$menu_entry" "$shortcut"
+                    ;;
+            esac
+        done
     done
 
     popd >/dev/null  # unnecessarily noisy
